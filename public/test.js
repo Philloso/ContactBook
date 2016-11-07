3ee53e68be6092dde790f2e8c173151fc57d6287
Philloso <PCherry4real@gmail.com>
2016-11-06 20:35:39 -0500

    new stuff

--- public/test.js
+++ public/test.js
@@ -1,48 +1,18 @@
-
-
-window.onload = function() {
- 
-//    alert( "welcome to Contact List App ;)" );
-    
-    $( ".imgtop" ).hide(-4);
-        $( ".img1" ).hide(2500);
-    
-    
-//   document.getElementById("my_audio").play();
-
-//$(document).ready(function() {
-   
-//});
-    
-};
-
-
-
-//window.onload = function() {
-//    document.getElementById("my_audio").play();
-//}
-//
-
-
 var audio = document.getElementsByTagName("audio")[0];
 var played = false;
 
+window.onload = function() {
+    $( ".imgtop" ).hide(-4);
+    $( ".img1" ).hide(2500);
+};
 
-     
 window.onclick = function(){
-       if (played) {
-//           audio.pause();
-           
+       if (played) {   
     $("#my_audio").pause();
-       }
+    }
       $(".imgtop").show(1800);
-    
-    $("#my_audio").get(0).play();
-     
- 
-    played = true;
-    
-    
+      $("#my_audio").get(0).play();
+       played = true;  
  }; 
 
 
