fee01dab35253903a3d9e8592a2d6f03cf65f029
Philloso <PCherry4real@gmail.com>
2016-11-06 21:50:20 -0500

    sound cloud

--- public/test.js
+++ public/test.js
@@ -1,6 +1,23 @@
 var audio = document.getElementsByTagName("audio")[0];
 var played = false;
 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+
+
+
+
+
 window.onload = function() {
     $( ".imgtop" ).hide(-4);
     $( ".img1" ).hide(2500);
