b3a2837c94b03bf1915427e048c002dcb0097308
Philloso <PCherry4real@gmail.com>
2016-11-17 22:33:24 -0500

    deleted files

--- public/test.js
+++ public/test.js
@@ -3,7 +3,7 @@ var played = false;
 
 var today = new Date();
 var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
+var mm = today.getMonth() + 1; //----January is 0!
 var yyyy = today.getFullYear();
 
 if (dd <10) {
@@ -21,9 +21,9 @@ window.onload = function() {
 
 window.onclick = function(){
        if (played) {   
-//    $("#my_audio").pause();
            
-       
+//----------------- $("#my_audio").pause();
+           
            $("#my_audio").pause();
        
     }
